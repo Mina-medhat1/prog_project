c733924503a95b899392c0f1ca0748951c3a37c1
shenoda <shenoda@DESKTOP-C26POO0>
2023-05-09 22:00:14 +0200

    githuhb

--- progproject/src/main/java/com/mycompany/progproject/main.java
+++ progproject/src/main/java/com/mycompany/progproject/main.java
@@ -2,7 +2,7 @@
 package com.mycompany.progproject;
 public class main {
     public static void main(String args[]){
-        System.out.println( "enter the nuber you like to find this key " );
+        System.out.println( "ez");
     
             }
 }
