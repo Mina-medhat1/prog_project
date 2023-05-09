cf060ca5e37f975dec95105eff270d3a994b95f2
shenoda <shenoda@DESKTOP-C26POO0>
2023-05-09 21:46:53 +0200

    githuhb

---
+++ progproject/src/main/java/com/mycompany/progproject/main.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.progproject;
+public class main {
+    public static void main(String args[]){
+        System.out.println( "enter the nuber you like to find this key " );
+    
+            }
+}
